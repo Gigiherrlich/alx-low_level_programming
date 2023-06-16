82de8bd728079d8ce63cd6124827e19e2ecbc93b
Gigiherrlich <geraldineonyekwuo.com>
2023-06-15 21:13:19 -0700

    Hello, World task done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
- * Return 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
-{ 
+{
 char a;
 int b;
 long int c;
